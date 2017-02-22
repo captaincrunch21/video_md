1a514254c4619e747f4a4091ac5bdc91b54a562e
captaincrunch21 <saiasdfgh@gmail.com>
2017-02-22 10:41:59 +0530

    edge detection by using X and Y direction lines hope for better computation in next commit

--- allfunctions.py
+++ allfunctions.py
@@ -14,7 +14,7 @@ def grayscale(image,size):
     return array
 a_lap=[0,-1,0,-1,4,-1,0,-1,0]
 a_mean=[1/9,1/9,1/9,1/9,1/9,1/9,1/9,1/9,1/9]
-def filter(image_array,tag='l'):
+def filter(image_array,tag='f'):
     m = len(image_array)
     n = len(image_array[0])
     a = [0 for _ in range(n)]
@@ -54,6 +54,67 @@ def filter(image_array,tag='l'):
                 filter_pointer = 0
         return array
 
+dir_x = [-1,-1,-1,2,2,2,-1,-1,-1]
+dir_y = [-1,2,-1,-1,2,-1,-1,2,-1]
+
+prewitt_x = [-1,-1,-1,0,0,0,-1,-1,-1]
+prewitt_y = [-1,0,-1,-1,0,-1,-1,0,-1]
+sobel_x = [-1,-2,-1,0,0,0,1,2,1]
+sobel_y = [-1,0,1,-2,0,2,-1,0,1]
 
 
 
+def linedetection(image_array,dir = 'x'):
+    m = len(image_array)
+    n = len(image_array[0])
+    a = [0 for _ in range(n)]
+    array = [a.copy() for _ in range(m)]
+
+    if dir=='x':
+
+        for image_row in range(m):
+            for image_col in range(n):
+                pix_range_i = [-1,-1,-1,0,0,0,1,1,1]
+                pix_range_j = [-1,0,1,-1,0,1,-1,0,1]
+                filter_pointer = 0
+                filtered_pixel_value = 0
+                for i , j in zip(pix_range_i,pix_range_j):
+                    try:
+                        filtered_pixel_value+=image_array[image_row+i,image_col+j] * sobel_x[filter_pointer]
+                        filter_pointer+=1
+                    except:
+                        filter_pointer+=1
+                array[image_row][image_col]=filtered_pixel_value
+                filter_pointer = 0
+        return array
+
+    if dir=='y':
+
+        for image_row in range(m):
+            for image_col in range(n):
+                pix_range_i = [-1,-1,-1,0,0,0,1,1,1]
+                pix_range_j = [-1,0,1,-1,0,1,-1,0,1]
+                filter_pointer = 0
+                filtered_pixel_value = 0
+                for i , j in zip(pix_range_i,pix_range_j):
+                    try:
+                        filtered_pixel_value+=image_array[image_row+i,image_col+j] * sobel_y[filter_pointer]
+                        filter_pointer+=1
+                    except:
+                        filter_pointer+=1
+                array[image_row][image_col]=filtered_pixel_value
+                filter_pointer = 0
+        return array
+
+def edgedetection(imagearray):
+    m = len(imagearray)
+    n = len(imagearray[0])
+
+    x_arr = linedetection(imagearray,'x')
+    y_arr = linedetection(imagearray,'y')
+    a = [0 for _ in range(n)]
+    array = [a.copy() for _ in range(m)]
+    for image_row in range(m):
+        for image_col in range(n):
+            array[image_row][image_col] = (x_arr[image_row][image_col] + y_arr[image_row][image_col])*0.5
+    return array
